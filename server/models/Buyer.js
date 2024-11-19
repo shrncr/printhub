5f550db71f091a3bccdf29bd549e2782b9579762
shrncr <loveyawink@gmail.com>
2024-11-19 09:35:13 -0600

    feat- added endpoints to login and to signup

--- server/models/Buyer.js
+++ server/models/Buyer.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const User = require('./User');
 
 const buyerSchema = new mongoose.Schema({
-  creditCard: { type: String, required: true },
-  address: { type: String, required: true }
+  creditCard: { type: String, required: false },
+  address: { type: String, required: false }
 }, { collection: 'Buyer' });
 
 module.exports = User.discriminator('Buyer', buyerSchema);
